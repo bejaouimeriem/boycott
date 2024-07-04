f698ed26923c490333c7b04fda9a14eb43376129
Mariem Bejaoui <bejaoui.meriyem@gmail.com>
2024-07-04 18:11:31 +0100

    Delete User

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php 
+
+    require_once('./db_connect/index.php');
+
+    $users=$pdo->prepare("select * from users");
+    $users->execute();
+
+    $res=$users->fetchAll();
+
+    include "./layout.php";
+
+
+?>
