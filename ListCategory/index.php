daa6ca5faa1112bf111bd5ce785b320d99be82e4
Mariem Bejaoui <bejaoui.meriyem@gmail.com>
2024-07-15 19:32:22 +0100

    listCategory

--- ListCategory/index.php
+++ ListCategory/index.php
@@ -1,7 +1,14 @@
-<?php 
+<?php
+require_once ('../db_connect/index.php');
 
- $page_titel="ListCategory";
- $template="ListCategory";
- include "../layout.phtml";
- 
- ?>
+$categories = $pdo->prepare("select * from categories");
+$categories->execute();
+$res = $categories->fetchAll();
+
+
+
+$page_titel = "ListCategory";
+$template = "ListCategory";
+include "../layout.phtml";
+
+?>
